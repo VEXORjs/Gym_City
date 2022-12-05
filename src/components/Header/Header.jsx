0dc6d8b5634e4318dfc84a886cf64297e761cc84
GHOSTY <kacperadam10@gmail.com>
2022-12-05 23:15:13 +0100

    small changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import './Header.css';
 import Logo from '../../assets/logo.png';
+import Bars from '../../assets/bars.png';
+import { useState } from 'react';
 
 const Header = () => {
+
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
+
   return (
     <div className="header">
       <img className='logo' src={Logo} alt="" />
-      <ul className='header-menu'>
-        <li>Główna</li>
-        <li>Sekcje</li>
-        <li>Dlaczego my?</li>
-        <li>Plany treningowe</li>
-        <li>Kontakt</li>
-      </ul>
-    </div>
-  )
+    {/*   {(menuOpened === false && mobile === true) ? (
+        <div><img src={Bars} alt="" ></img>
+      ): } */}
+          <ul className='header-menu'>
+            <li>Główna</li>
+            <li>Sekcje</li>
+            <li>Dlaczego my?</li>
+            <li>Plany treningowe</li>
+            <li>Kontakt</li>
+          </ul>
+        </div>
+      )
 }
 
-export default Header
+      export default Header
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,11 +5,12 @@ import Heart from '../../assets/heart.png';
 import hero_image from '../../assets/hero_image.png';
 import hero_image_back from '../../assets/hero_image_back.png';
 import Calories from '../../assets/calories.png';
-
+import NumberCounter from 'number-counter';
 import { motion } from 'framer-motion';
 
 const Hero = () => {
     const transition = { type: 'spring', duration: 3 }
+    const mobile = window.innerWidth <= 768 ? true : false;
 
     return (
         <div className="hero">
@@ -18,7 +19,7 @@ const Hero = () => {
                 <Header />
                 <div className='the-best-ad'>
                     <motion.div
-                        initial={{ left: '238px' }}
+                        initial={{ left: mobile ? '160px' : '238px' }}
                         whileInView={{ left: '8px' }}
                         transition={{ ...transition, type: 'tween' }}
                     >
@@ -41,15 +42,21 @@ const Hero = () => {
                 </div>
                 <div className="figures">
                     <div>
-                        <span>3424</span>
+                        <span>
+                            <NumberCounter end={150} start={50} delay='4' preFix='+' />
+                        </span>
                         <span>wspierajacych</span>
                     </div>
                     <div>
-                        <span>232323</span>
+                        <span>
+                            <NumberCounter end={45} start={5} delay='4' preFix='+' />
+                        </span>
                         <span>trenerow</span>
                     </div>
                     <div>
-                        <span>454675</span>
+                        <span>
+                            <NumberCounter end={1150} start={250} delay='2.5' preFix='+' />
+                        </span>
                         <span>uczestnikow</span>
                     </div>
                 </div>
