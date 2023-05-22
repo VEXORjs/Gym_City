45ef3783e34bfdc8d35a0601f4deb93838e84c5c
GHOSTY <kacperadam10@gmail.com>
2023-05-23 00:15:57 +0200

    Some fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import Logo from '../../assets/logo.png';
 import './Header.css';
+import { HashRouter, NavLink, Route } from "react-router-dom";
+import Programs from '../Programs/Programs';
+import Reasons from '../Reasons/Reasons';
+import Plans from '../Plans/Plans';
+import Testimonials from '../Testimonials/Testimonials';
+import Hero from '../Hero/Hero';
 
 const Header = () => {
   const handleClick = (e) => {
@@ -31,16 +37,28 @@ const Header = () => {
   }
 
   return (
-    <div className="header">
+    <>
       <img onClick={handleClick} className='logo' src={Logo} alt="" />
-      <ul className='header-menu'>
-        <li href="#">Główna</li>
-        <li>Sekcje treningowe</li>
-        <li>Dlaczego my?</li>
-        <li>Plany treningowe</li>
-        <li>Kontakt</li>
-      </ul>
-    </div>
+      <div className="header">
+      </div>
+
+    </>
+/*      <HashRouter>*/
+        /*  <ul className='header-menu'>
+          <li><NavLink exact to='/'>Główna</NavLink></li>
+          <li><NavLink to="/programs">Sekcje treningowe</NavLink></li>
+          <li><NavLink to="/reasons">Dlaczego my?</NavLink></li>
+          <li><NavLink to="/plans">Plany treningowe</NavLink></li>
+          <li><NavLink to="/testimonials">Kontakt</NavLink></li>
+        </ul> */
+  /*       <div>
+          <Route exact path='/' Component={Hero} />
+          <Route path='programs' Component={Programs} />
+          <Route path='/reasons' Component={Reasons} />
+          <Route path='/plans' Component={Plans} />
+          <Route path='/testimonials' Component={Testimonials} />
+        </div> */
+ /*    </HashRouter>  */
   )
 }
 
